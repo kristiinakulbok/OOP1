711b36143ed4c168381d9f6ca2f39638759b68ac
kristiinakulbok <kristiina.kulbok@gmail.com>
2019-04-02 16:27:22 +0300

    pisivigade ja loetavuse parandus

--- src/Peaklass.java
+++ src/Peaklass.java
@@ -1,3 +1,8 @@
+// tegemata: case 2 (ülesande tehtuks märkimine) või on see üleliigne, sest tegelikult saavutab sama asja ka
+// ülesande nimekirjast eemaldamisega?
+// konstruktorid, get ja set meetodid
+// ülesannete prioritiseerimine
+
 import java.io.File;
 import java.util.Scanner;
 
@@ -6,79 +11,83 @@ public class Peaklass {
     public static void main(String[] args) {
         File fail = new File("todolist.txt");
         ToDoList toDoList = new ToDoList();
-        if (fail.exists()){
+        if (fail.exists()) {
             toDoList.lae(fail);
-        }
-        else{
+        } else {
             try {
                 fail.createNewFile();
-            }
-            catch (Exception e){
+            } catch (Exception e) {
                 System.out.println("Tekkis viga faili loomisel");
             }
         }
         System.out.println("Tere! Oled kasutamas to-do list'i ehk täitmist vajavate ülesannete nimekirja.");
+        System.out.println("Sisesta 1, et lisada uus ülesanne");
+        System.out.println("Sisesta 2, et märkida mingi ülesanne tehtuks");
+        System.out.println("Sisesta 3, et eemaldada mingi ülesanne nimekirjast");
+        System.out.println("Sisesta 4, et lasta fortuunal otsustada järgmine tegevus");
+        System.out.println("Sisesta 5, et kuvada kõik hetkel nimekirjas olevad ülesanded");
+        System.out.println("Sisesta 6, et salvestada praegune to-do list");
+        System.out.println("Sisesta 7, et programmist väljuda");
+
         boolean loop = true; // selle jaoks, et programm töötaks peale ühe käsu täitmist.
-        boolean salvestatud = true; // see praegu ei tee midagi, aga tulevikus kontrollib kas on salvestatud või mitte
-                                    // enne, kui programmist väljutakse.
+        boolean salvestatud = true;
+
         while (loop) {
             int valik = 0;
-
-            System.out.println("Sisesta 1, et lisada uus ülesanne");
-            System.out.println("Sisesta 2, et märkida mingi ülesanne tehtuks");
-            System.out.println("Sisesta 3, et eemaldada mingi ülesanne nimekirjast");
-            System.out.println("Sisesta 4, et lasta fortuunal otsustada järgmine tegevus");
-            System.out.println("Sisesta 5, et kuvada kõik hetkel nimekirjas olevad ülesanded");
-            System.out.println("Sisesta 6, et salvestada praegune to-do list");
-            System.out.println("Sisesta 7, et programmist väljuda");
 
             try {
                 Scanner kasutajaSisend = new Scanner(System.in);
                 System.out.println("Mida soovid järgmiseks teha? ");
                 valik = Integer.parseInt(kasutajaSisend.nextLine());
-            }
-            catch(Exception e){
+                System.out.println();
+            } catch (Exception e) {
                 System.out.println("Vigane sisend, sisesta vaid number.");
+                System.out.println();
             }
 
             //Tegin selle osa korda for loopiga, nüüd programm jääb käima
-            //
 
             switch (valik) {
                 case 1:
                     toDoList.lisaUusKirje();
+                    toDoList.eraldaja();
                     salvestatud = false;
                     break;
 
                 case 2:
                     System.out.println("null"); // ei tee midagi
+                    toDoList.eraldaja();
                     salvestatud = false;
                     break;
 
                 case 3:
                     toDoList.eemaldaÜlesanne();
+                    toDoList.eraldaja();
                     salvestatud = false;
                     break;
 
                 case 4:
                     toDoList.loosiÜlesanne();
+                    toDoList.eraldaja();
                     break;
 
                 case 5:
                     toDoList.väljastaLisatudKirjed();
+                    toDoList.eraldaja();
                     break;
 
                 case 6:
                     toDoList.salvesta(fail);
+                    toDoList.eraldaja();
                     salvestatud = true;
                     break;
 
                 case 7:
-                    if (!salvestatud){
+                    if (!salvestatud) {
                         System.out.println("Praegune list on salvestamata!");
                         break;
-                    }
-                    else {
+                    } else {
+                        System.out.println("Oled programmist väljunud. Hüvasti!");
                         loop = false;
                     }
             }
--- src/ToDoList.java
+++ src/ToDoList.java
@@ -6,81 +6,96 @@ public class ToDoList {
 
     ArrayList<String> toDoList = new ArrayList<String>();
 
+    //case 1
     public void lisaUusKirje() {
         Scanner sisend = new Scanner(System.in);
         System.out.println("Mida soovid lisada? ");
         String ülesanne = sisend.nextLine();
         toDoList.add(ülesanne);
+        System.out.println("Uus kirje lisatud!");
     }
 
-    public void väljastaLisatudKirjed() {
-        if (toDoList.isEmpty()) {
-            System.out.println("List on tühi.");
-        }
-        else {
-            listiPrint();
-        }
-    }
-
-    private void listiPrint() { // eemaldasin duplikaat koodi, selle eest kaotab punkte vist
-        System.out.println("Listis on " + toDoList.size() + " kirjet: ");
-        for (int i = 0; i < toDoList.size(); i++) {
-            System.out.println(i + 1 + ". " + toDoList.get(i));
-        }
-    }
-
-    public String loosiÜlesanne() {
-        if (toDoList.isEmpty()) {
-            return "List on tühi. Sul ei ole midagi teha.";
-        }
-        else {
-            int max = toDoList.size();
-            int juhuslikArv = (int) (Math.random() * max);
-            System.out.print("Fortuuna valis: ");
-            return toDoList.get(juhuslikArv);
-        }
-    }
-
+    //case 3
     public String eemaldaÜlesanne() {
         if (toDoList.isEmpty()) {
-            return "See list on juba tühi.";
-        }
-        else {
+            System.out.println("See list on juba tühi.");
+            return null; //kohmakas lahendus, aga tagastustüüp void ei sobinud rekursiooniga
+        } else {
             listiPrint();
             Scanner eemaldatav = new Scanner(System.in);
             System.out.println("Sisesta ülesande number, mida soovid eemaldada: ");
             try {
                 int number = Integer.parseInt(eemaldatav.nextLine());
                 toDoList.remove(number - 1);
-                return "Ülesanne number " + number + " edukalt eemaldatud.";
-            }
-            catch(Exception e){
+                System.out.println("Ülesanne number " + number + " edukalt eemaldatud.");
+                return null;
+            } catch (Exception e) {
                 System.out.println("Vigane sisend, sisesta vaid number.");
                 return eemaldaÜlesanne();
             }
         }
     }
-        // uued meetodid, salvestavad ja laadivad failist to-do listi.
+
+    //case 4
+    public void loosiÜlesanne() {
+        if (toDoList.isEmpty()) {
+            System.out.println("List on tühi. Sul ei ole midagi teha.");
+        } else {
+            int max = toDoList.size();
+            int juhuslikArv = (int) (Math.random() * max);
+            System.out.print("Fortuuna valis: ");
+            System.out.println(toDoList.get(juhuslikArv));
+        }
+    }
+
+    //case 5
+    public void väljastaLisatudKirjed() {
+        if (toDoList.isEmpty()) {
+            System.out.println("List on tühi.");
+        } else {
+            listiPrint();
+        }
+    }
+
+    //case 6
     public void salvesta(File fail) {
         try (java.io.PrintWriter pw = new java.io.PrintWriter(fail, "UTF-8")) {
             for (int i = 0; i < toDoList.size(); i++) {
                 pw.println(toDoList.get(i));
             }
-        }
-        catch (Exception e){
+            System.out.println("Ülesannete nimekiri salvestatud.");
+        } catch (Exception e) {
             System.out.println("Tekkis viga faili salvestamisel.");
         }
     }
 
-    public void lae(File fail){
-        try(java.util.Scanner sc = new java.util.Scanner(fail, "UTF-8")){
-            while (sc.hasNextLine()){
+    private void listiPrint() { // eemaldasin duplikaat koodi, selle eest kaotab punkte vist
+        System.out.println("Listis on " + toDoList.size() + " kirje(t): ");
+        for (int i = 0; i < toDoList.size(); i++) {
+            System.out.println(i + 1 + ". " + toDoList.get(i));
+        }
+    }
+
+    public void lae(File fail) {
+        try (java.util.Scanner sc = new java.util.Scanner(fail, "UTF-8")) {
+            while (sc.hasNextLine()) {
                 String rida = sc.nextLine();
                 toDoList.add(rida);
             }
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Tekkis viga faili lugemisel.");
         }
+    }
+
+    //loetavuse parandamiseks
+    public void eraldaja() {
+        String s = "*";
+        int n = 64;
+        System.out.println();
+        StringBuilder sb = new StringBuilder(s.length() * n);
+        for (int i = 0; i < n; i++)
+            sb.append(s);
+        System.out.println(sb.toString());
+        System.out.println();
     }
 }
